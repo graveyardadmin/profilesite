d3c622851c4d4b6382d9dd7e931c165986b4d12c
graveyardadmin <valdemar@3x00.net>
2023-02-10 15:08:55 +0200

    Fixed some styling bugs

--- pages/users/steph.js
+++ pages/users/steph.js
@@ -1,6 +1,6 @@
 export default function Steph() {
   return(
-    <div className="center">
+    <div className="center template">
       <h1 className="magic">Hello!</h1>
 
       <p>Hi I'm Steph and I'm a loser.</p>
