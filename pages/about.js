754b3570a60f77f22b4174191ab424c36a3591f1
graveyardadmin <valdemar@3x00.net>
2023-02-11 12:52:40 +0200

    Added no-bg class. Added template background class.

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,13 @@
 import HomeButton from "@/components/HomeButton";
+import Head from "next/head";
 
 export default function About() {
   return( 
-    <div className="template">
+    <div className="template no-bg">
+      <Head>
+        <title>About 3x00</title>
+      </Head>
+
       <section className="template-section">
         <HomeButton/>
 
@@ -18,59 +23,37 @@ export default function About() {
 
         <h2 className="magic">How do I make my own site?</h2>
 
-        <p>It's actually quite simble and will only take you about an hour!</p>
+        <p>
+          For a more in-depth guide on how to create your own site,
+          refer to 
+          our <a href="https://github.com/graveyardadmin/profilesite" rel="noreferrer" target="_blank">
+            GitHub Page.
+          </a>
+          <br/>
 
-        <h3>If you don't know Next.JS</h3>
-        <ol>
-          <li>
-            Go to <a href="https://github.com/graveyardadmin/profilesite">this</a> GitHub
-            repository and clone the users branch to your computer.
-          </li>
-          <li>
-            Open the {`"template.md"`} file from the {`"./mds"`} folder and save it
-            as {`"{your desired username}.md"`}. <br/><b>Note:</b> your application
-            won't be accepted if you delete someone elses file.
-          </li>
-          <li>
-            Commit the changes to your branch that you copied and create a pull
-            request for your changes.
-          </li>
-        </ol>
-
-        <h3>If you know Next.JS</h3>
-        <ol>
-          <li>
-            Go to <a href="https://github.com/graveyardadmin/profilesite">this</a> GitHub
-            repository and clone the users branch to your computer.
-          </li>
-          <li>
-            Open the {`"./users/"`} folder, and create a file with your desired name.
-            <br/><b>Note:</b> your application won't be accepted if you delete
-            someone elses file.
-          </li>
-          <li>
-            Open the {`"./styles/user/"`} folder, and create a {`".module.css"`}
-            file with the same name as the function that you exported
-            from your react page.
-          </li>
-          <li>
-            Create your own fully custom Next.js webpage.
-          </li>
-          <li>
-            Commit the changes to your branch that you copied and create
-             a pull request for your changes.
-          </li>
-        </ol>
+          But here's the TLDR. Fork our GitHub repository, create a React page in
+          the {`"./users/" `} directory with your desired name {`(that has not already been taken) `}
+          and create a .module.css file with the same name as the React page that you created. 
+          Then you can make the website exactly how you'd like.
+        </p>
 
         <hr/>
 
-        <h2 className="magic">Can I contribute?</h2>
-        <p>If you spot a tpyo or have any suggestions, you can create an issue on our <a href="https://github.com/graveyardadmin/profilesite/issues/new" rel="noreferrer" target="_blank">GitHub Repository</a></p>
+        <h2 className="magic">
+          Can I contribute?
+        </h2>
+
+        <p>
+          If you spot a tpyo or have any suggestions,
+          you can create an issue on 
+          our <a href="https://github.com/graveyardadmin/profilesite/issues/new" rel="noreferrer" target="_blank">
+            GitHub Repository
+          </a>
+        </p>
       </section>
 
-      <section className="template-section">
-
-      </section>
+      <div className='backgrounder'>
+      </div>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,28 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 export default function Home() {
-  return (
-    <div className="template center-section">
-      <section className='template-section center'>
-        <Head>
-          <title>3x00</title>
-        </Head>
+  return (      
+      <div className="template center-section no-bg">
+        <section className='template-section center'>
+          <Head>
+            <title>3x00</title>
+          </Head>
 
-        <h1>
-          <i className="orange">Fully</i> customisable
-          single page websites,
-          for <b className="magic">free.</b>
-        </h1>
+          <h1>
+            <i className="orange">Fully</i> customisable
+            single page websites,
+            for <b className="magic">free.</b>
+          </h1>
 
-        <p>.. did I mention it's open source?</p>
+          <p>.. did I mention it's open source?</p>
 
-        <br/>
-        
-        <Link href="/about"><a>Read more.</a></Link>
-      </section>
+          <br/>
 
+          <Link href="/about"><a>Read more.</a></Link>
+        </section>
+
+        <div className='backgrounder'>
+        </div>
     </div>
   )
 }
