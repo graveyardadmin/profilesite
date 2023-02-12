f690bd7cf7e021f31b0b0cecf95661d648040378
graveyardadmin <valdemar@3x00.net>
2023-02-12 15:00:27 +0200

    Added h1 top margin. Removed template-section bottom padding. Modified docs homepage to have background class.

---
+++ pages/docs/components.js
@@ -0,0 +1,11 @@
+export default function Components() {
+  return(
+    <div className="template">
+      <section className="template-section background">
+        <h1 className="magic">help me</h1>
+
+        <p>bitch</p>
+      </section>
+    </div>
+  );
+}
--- pages/docs/index.js
+++ pages/docs/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Docs() {
   return(
     <div className="template">
-      <section className="template-section">
+      <section className="template-section background">
         <h1 className="magic">Docs</h1>
 
         <p>
